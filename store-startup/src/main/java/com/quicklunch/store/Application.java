d1cef8cc92126cccaa6e51ff0c25f9e7eb4c2024
JiWen <jiwen.chen@lkcoffee.com>
2025-11-15 23:23:01 +0100

    Dockerfile

--- store-startup/src/main/java/com/quicklunch/store/Application.java
+++ store-startup/src/main/java/com/quicklunch/store/Application.java
@@ -1,7 +1,8 @@
-package com.quicklunch.store.startup;
+package com.quicklunch.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
 public class Application {
